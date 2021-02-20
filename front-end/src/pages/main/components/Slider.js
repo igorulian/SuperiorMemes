f06904ae4d144f24770aefa16cafc68d2a290686
Igor Ulian <igorulian@outlook.com>
2021-02-20 16:36:12 -0300

    English translate
    File name changed
    English translate

---
+++ front-end/src/pages/main/components/Slider.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import './Slider.css'
+import TinderCard from 'react-tinder-card'
+import * as AiContext from 'react-icons/ai'
+import { IconContext } from "react-icons";
+import api from '../../../services/api'
+
+export default class Slider extends Component{
+
+    state = {
+        meme: []
+    }
+
+    componentDidMount() {
+        this.loadRequests()
+    }
+
+    loadRequests = async() => {
+        try{
+            const response = await api.get(`/list`)
+            this.setState({meme: response.data.docs})
+        }catch{
+            console.log("Erro ao carregar produtos")
+            alert("Erro ao carregar produtos")
+        }
+    }
+
+    onSwipe = async(direction, memeid) => {
+        var rate = 0
+        direction == 'right' ? rate = 1 : rate = 0
+
+        await api.post(`rate/${rate}/${memeid}`)
+
+        await api.post('rate/' + rate + '/' + memeid)
+
+
+    }
+       
+    onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+    //<TinderCard onSwipe={this.onSwipe(memeid=meme._id)} onCardLeftScreen={() => this.onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']}>
+
+    render(){
+        return (
+            <>
+            {console.log(this.state.meme)}
+            {this.state.meme.map((meme) => (
+                <div className="swipe-container">
+                    <TinderCard onSwipe={(direction, memeid=meme._id) => {this.onSwipe(direction,memeid)}} onCardLeftScreen={() => this.onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']}>
+                        <div className="swipe-content">
+                            <img src={meme.imageUrl} />
+
+                            <div className="linha-horizontal"/> 
+
+                            <div className="swipe-content-footer">
+                            
+                                <div className="swipe-content-button-like">
+                                    <IconContext.Provider value={{color: "#006eff", size: '40'}}>
+                                        <AiContext.AiFillLike/>
+                                    </IconContext.Provider>
+                                    <p>{meme.likes}</p>
+                                </div>
+
+                                <div className="swipe-content-button-dislike">
+                                    <IconContext.Provider value={{color: "#006eff", className: "global-class-name", size: '40'}}>
+                                        <AiContext.AiOutlineDislike/>
+                                    </IconContext.Provider>
+                                    <p>{meme.dislikes}</p>
+                                </div>
+
+                                <p style={{paddingTop: '40px', color: '#DDD'}}> @{meme.publisherName}</p>
+
+                            </div>
+                        </div>
+                    </TinderCard>
+                </div>
+            ))}
+            </>
+        )
+
+    }
+}
---
+++ front-end/src/pages/upload/upload.js
@@ -0,0 +1,144 @@
+import './upload.css'
+import React, {Component} from 'react';
+import '../../styles.css'
+import {DropContainer,UploadMessage} from './image-upload-components.js'
+import Dropzone from 'react-dropzone'
+import api from '../../services/api'
+
+export default class Upload extends Component{
+
+    state = {
+        uploadingImageUrl: '',
+        sending: false
+
+    }
+
+    renderDrageMessage = (isDragActive, isDragReject) => {
+        if (!isDragActive){
+            return <UploadMessage style={{fontSize: '30px'}}> Drag your image here</UploadMessage>
+        }
+        if(isDragReject){
+            return <UploadMessage type="error" style={{fontSize: '30px'}}> File not suported </UploadMessage>
+        }
+
+        return <UploadMessage type="sucess"> Drop your image here </UploadMessage>
+    }
+
+    showImageToBeSent  = async (file) => {
+        try{
+            //URL.createObjectURL(file) // URL.createObjectURL(file[0])
+            const imageUrl = URL.createObjectURL(file[0])
+            this.setState({uploadingImageUrl: imageUrl})
+
+            const image = new FormData()
+
+            console.log(file[0])
+
+            image.append('file', file[0], file[0].name)
+
+            this.setState({uploadedImage: image})
+
+        }catch{
+            alert('Error send preview image')
+        }
+
+    }
+
+    cancelImageUpload = () => {
+        this.setState({uploadingImageUrl: '', uploadedImage: ''})
+    }
+
+    uploadMeme = async () => {
+
+        this.setState({...this.state, sending: true})
+
+        const image = this.state.uploadedImage
+
+        if(!image){alert('You have to send a image to upload your meme'); this.resetVariables(); return;}
+
+        var imageUrl = ''
+
+        await api.post(`/upload/image`,image).then(res => {
+            imageUrl = res.data.location
+        }).catch(err => {
+            console.log(err)
+            alert('Error sending image, try a smaller one')
+            this.resetVariables()
+        })
+
+        if(imageUrl == ''){alert('Error sending image, try again later'); this.resetVariables(); return;}
+
+        const publisherName = this.publisherNameInput.value
+
+        const meme = {
+            publisherName,
+            imageUrl,
+        }
+
+        console.log(meme)
+        if(!publisherName){alert('Type your publisher name'); this.resetLoading(); return;}
+
+        await api.post(`/upload`,meme).then(res => {
+            console.log(res)
+            this.uploadSucessfull()
+        }).catch(err => {
+            console.log(err)
+            alert('Error sending meme, try again later')
+            this.resetVariables();
+        })
+
+        this.resetVariables()
+    }
+
+    resetVariables = () => {
+        this.setState({uploadingImageUrl: '',uploadedImage: '', uploadedImageUrl: '', sending: false})
+    }
+
+    resetLoading = () => {
+        this.setState({...this.state, sending: false})
+    }
+
+    uploadSucessfull = () => {
+        alert('Upload Sucessfull!')
+        window.location.href = '/'
+    }
+
+    render(){
+        return(   
+            <div className="page">
+                <h1> Upload </h1>
+                <div className="container-upload-image">
+                    <Dropzone accept="image/*" onDropAccepted={(file) => this.showImageToBeSent(file)}>
+                        { ({getRootProps, getInputProps, isDragActive, isDragReject}) => (
+                            this.state.uploadingImageUrl == '' ?
+                            <DropContainer {...getRootProps()}
+                            isDragActive={isDragActive}
+                            isDragReject={isDragReject}
+                            >
+                            
+
+                            <input {...getInputProps()}/>
+
+                            {this.renderDrageMessage(isDragActive, isDragReject)}
+
+                            </DropContainer>
+                            : 
+                            <div>
+                                <button className="btnCancelarEnvioImagem" style={{marginRight: '0px'}} onClick={() => this.cancelImageUpload()}>x </button>
+                                <img src={this.state.uploadingImageUrl} style={{width: '280px', height: '280px', border: '2px solid #006eff', borderRadius: '5px'}}/>
+                            </div>
+                        )}
+                    </Dropzone>
+                    <div className="publisher-name">
+                        <p> Publisher: </p>
+                        <input ref={input => this.publisherNameInput = input} />
+                    </div>
+                    <button className="btnUpload" onClick={() => this.uploadMeme()}> {this.state.sending == false ? <b>Send meme</b> : <b>Sending...</b>} </button>
+                </div>
+
+            </div>
+        )
+    }
+
+
+}
