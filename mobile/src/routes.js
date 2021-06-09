7572e17aecbf22b4878359d48407a7f7305d5469
Igor Ulian <igorulian@outlook.com>
2021-06-09 12:10:26 -0300

    separated routes in routes.js file
    - cleaner app.js :)

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,67 @@
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto'
+import Home from './pages/home/home'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator();
+
+const iconSize = 30
 
 
-
-const TabRoutes = () => (
-    <>
-    </>
+export const TabRoutes = () => (
+    <Tab.Navigator initialRouteName="Home" tabBarOptions={{style: {borderTopWidth: 0},activeTintColor: '#faf601', inactiveTintColor: '#faf60166' , showLabel: false ,tabStyle:{backgroundColor: '#282a2e'}}}>
+        <Tab.Screen
+          name="Profile"
+          component={Home}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="user" color={color} size={iconSize}/>
+            ),
+          }}  
+        />
+        <Tab.Screen
+          name="Upload"
+          component={Home}
+          options={{
+            tabBarLabel: 'Upload',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="upload" color={color} size={iconSize + 5} />
+            ),
+          }}  
+        />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="duck" color={color} size={iconSize + 10} />
+            ),
+          }}  
+        />
+        <Tab.Screen
+          name="Like"
+          component={Home}
+          options={{
+            tabBarLabel: 'Like',
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="heart" color={color} size={iconSize} />
+            ),
+          }}  
+        />
+        <Tab.Screen
+          name="Share"
+          component={Home}
+          options={{
+            tabBarLabel: 'Share',
+            tabBarIcon: ({ color }) => (
+              <Fontisto name="share-a" color={color} size={iconSize} />
+            ),
+          }}  
+        />
+   </Tab.Navigator>
 )
