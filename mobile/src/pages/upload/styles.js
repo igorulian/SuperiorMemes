c01c622cb0becb91adf1f1e3efee4384fa95babe
Igor Ulian <igorulian@outlook.com>
2021-06-14 10:29:07 -0300

    starting upload page

--- mobile/src/pages/upload/styles.js
+++ mobile/src/pages/upload/styles.js
@@ -14,5 +14,48 @@ export const styles = StyleSheet.create({
         marginBottom: 20,
         top: 0,
         position: 'absolute'
+    },
+    uploadContainer:{
+        marginBottom: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    uploadContent:{
+        width: 300,
+        height: 300,
+        borderColor: '#faf601',
+        borderStyle: 'dotted',
+        borderWidth: 4,
+        borderRadius: 0.1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    uploadButton:{
+        width: 200,
+        height: 45,
+        backgroundColor: '#faf601',    
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    uploadButtonText:{
+        color: '#000',
+        fontSize: 20
+    },
+    descriptionContainer:{
+        marginTop: 20
+    },
+    descriptionInput:{
+        borderWidth: 2,
+        borderColor: '#faf601',
+        padding: 5,
+        paddingLeft: 10,
+        width: 300,
+        height: 40,
+        color: '#fff',
+        marginTop: 5
+    },
+    descriptionTxt:{
+        color: '#faf601'
     }
 })
--- mobile/src/pages/upload/upload.js
+++ mobile/src/pages/upload/upload.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import {styles} from './styles'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class Upload extends Component {
 
@@ -10,6 +11,23 @@ export default class Upload extends Component {
                 <Text style={styles.title}>
                     Upload 
                 </Text>
+
+                <View style={styles.uploadContainer}>
+                    <TouchableOpacity style={styles.uploadContent}>
+                        <MaterialCommunityIcons name="upload" color={'#faf016'} size={100} />
+                    </TouchableOpacity>
+
+                    <View style={styles.descriptionContainer}>
+                        <Text style={styles.descriptionTxt}> Description </Text>
+                        <TextInput style={styles.descriptionInput} textCo/>
+                    </View>
+
+
+                    <TouchableOpacity style={styles.uploadButton}>
+                        <Text style={styles.uploadButtonText}>  Publish Meme </Text>
+                    </TouchableOpacity>
+
+                </View>
             </View>
         )
     }
