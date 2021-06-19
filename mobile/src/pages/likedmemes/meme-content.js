2fbfe2bf215d24f68597f384214a1ded2ca0006c
Igor Ulian <igorulian@outlook.com>
2021-06-19 00:25:22 -0300

    fixed video display on liked memes page

--- mobile/src/pages/likedmemes/meme-content.js
+++ mobile/src/pages/likedmemes/meme-content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity, Alert } from 'react-native';
 import {styles} from './meme-style'
 import { useNavigation } from '@react-navigation/native';
+import Video from 'react-native-video';
 
 
 export default function MemeContent(props){
@@ -12,7 +13,10 @@ export default function MemeContent(props){
     return (
         <TouchableOpacity style={styles.memecontainer} onPress={() => {navigation.navigate('Like', {screen: 'MemeIndividual', params: { meme }})}}>
             <View style={styles.memecontent}>
+                {meme.mimetype.includes('video') ?
+                <Video style={styles.memeImage} paused={true} source={{ uri: meme.imageUrl }}/> :
                 <Image style={styles.memeImage} source={{ uri: meme.imageUrl }}/>
+                }
 
                 <View style={styles.memefooter}>
                     <Text style={styles.memeDescription}> {meme.description} </Text>
